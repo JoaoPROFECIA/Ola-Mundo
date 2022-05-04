071da385f93e9b652864c72226f3ef5afc15ba37
Joao Victor <victorjv3288@gmail.com>
2022-05-03 21:30:21 -0300

    jupyter lab

---
+++ site-exemplo/desafio/meu_primeiro_notebook.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 4
+print(f'{x} ao quadrado é igual à {x**2}')
+
+
+# # Meu título
+
+# Minha lista:
+# - item 1
+# - item 2
+# - item 3
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
